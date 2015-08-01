ca53026dfb459471ee444d46261c306c90c9f7c3
michaelgrasso <michael.grasso1@my.jcu.edu.au>
2015-08-01 17:13:54 +1000

    Lab 1
    CC3501 Lab 1

---
+++ Lab1/Sources/main.c
@@ -0,0 +1,105 @@
+/* ###################################################################
+**     Filename    : main.c
+**     Project     : Lab1
+**     Processor   : MK20DX128VLH5
+**     Version     : Driver 01.01
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2015-07-22, 10:52, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file main.c
+** @version 01.01
+** @brief
+**         Main module.
+**         This module contains user's application code.
+*/         
+/*!
+**  @addtogroup main_module main module documentation
+**  @{
+*/         
+/* MODULE main */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "RedPWM.h"
+#include "PwmLdd1.h"
+#include "TU1.h"
+#include "GreenLED.h"
+#include "BitIoLdd1.h"
+#include "BlueLED.h"
+#include "BitIoLdd2.h"
+#include "Timer.h"
+#include "RealTimeLdd1.h"
+#include "TU2.h"
+#include "AD1.h"
+#include "AdcLdd1.h"
+/* Including shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+#include <math.h>
+
+/*lint -save  -e970 Disable MISRA rule (6.3) checking. */
+int main(void)
+/*lint -restore Enable MISRA rule (6.3) checking. */
+{
+  /* Write your local variable definition here */
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+AD1_Calibrate(TRUE);
+  /* Write your code here */
+  for (;;) {
+	  // Set the PWM ratio on the Red LED
+	  // word time;
+	  //Timer_GetTimeMS(&time); // Get time in milliseconds
+	  //RedPWM_SetRatio16(0xFFFF * fabs(cos(time / 2000.0 * 2 * 3.14))); // Range is 0 to 0xFFFF
+
+
+	 	  AD1_Measure(TRUE);
+	 	  uint8 adc_result;
+	 	  AD1_GetValue8(&adc_result);
+	 	  RedPWM_SetRatio8(adc_result);
+
+
+
+  }
+
+
+
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END main */
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
